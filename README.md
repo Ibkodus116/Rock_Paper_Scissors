45d069e1c26d69310d3b4a51c6c93c7b79d162a8
Dogged Mind <39561119+Ibkodus116@users.noreply.github.com>
2022-06-01 08:37:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rock-Paper-Scissors Game
+will be building the Game Rock Paper Scissors
+this is a two man game where each person get to pick between rock, paper and scissors
+paper win over rock, rock wins over scissors, scissors wins over paper
+this is just a practice project and a task from Zuri/i4g training 
+i aim at understanding python more with this
+i will be making use of the following syntax
+- loop          used
+- funcion       used 
+- List          used
+# Tought Process
+- i made use of an inbuilt function called "random.choice()" to choose at random from the given *List* in other to choose for computer 
+- Then I created two functions called *check_player()* and *check_computer()* that checks if the player and computer picks [Rock, Paper, Scissors]
+- after which the *game()* function will check and compare what was picked by each player. and print out the winner.
+- the *game()* function will keep running if invalid option is inserted or the players picked the same value.
---
+++ main.py
@@ -0,0 +1,77 @@
+import random
+
+computer = ['R', 'P', 'S']
+computer_pick = random.choice(computer)
+p = 'Paper'
+r = 'Rock'
+s = 'Scissors'
+
+
+def game():
+    pick_status = False
+
+    while pick_status == False:
+        player_pick = input(
+            'choose between [Rock] R, [Paper] P, [Scissors] S \n').upper()
+
+        if check_computer(player_pick) == check_player(computer_pick):
+            # pick_status = False
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print('No winner this is a draw')
+            print('----------------------------------------------')
+
+        elif check_computer(player_pick) == p and check_player(computer_pick) == r:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("player won this round")
+        elif check_computer(player_pick) == p and check_player(computer_pick) == s:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("Computer won this round")
+        elif check_computer(player_pick) == r and check_player(computer_pick) == p:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("Computer won this round")
+        elif check_computer(player_pick) == r and check_player(computer_pick) == s:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("Player won this round")
+        elif check_computer(player_pick) == s and check_player(computer_pick) == r:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("Computer won this round")
+        elif check_computer(player_pick) == s and check_player(computer_pick) == p:
+            pick_status = True
+            print("Player (%s) : CPU (%s)" %
+                  (check_computer(player_pick), check_player(computer_pick)))
+            print("Player won this round")
+
+        else:
+            print("you've Picked an invalid option please try again")
+
+
+def check_player(player):
+    if player == 'R':
+        return r
+    if player == 'P':
+        return p
+    if player == 'S':
+        return s
+
+
+def check_computer(computer):
+    if computer == 'R':
+        return r
+    if computer == 'P':
+        return p
+    if computer == 'S':
+        return s
+
+
+game()
